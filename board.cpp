4339cdf5cae69a9e6b9160389b571aa6b5ca80ca
RyRy-Winner <ryker.ballash@gmail.com>
2026-04-15 08:30:32 -0600

    Added board

---
+++ board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rbal7885 on 4/15/2026.
+//
+
+#include "board.h"
---
+++ board.h
@@ -0,0 +1,17 @@
+//
+// Created by rbal7885 on 4/15/2026.
+//
+
+#ifndef CENGINE_BOARD_H
+#define CENGINE_BOARD_H
+
+
+
+class board {
+public:
+
+};
+
+
+
+#endif //CENGINE_BOARD_H
